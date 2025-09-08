e6cf2301bf091b80dcd58ca118ff59d4c5dbdcb6
unknown <3345920771@qq.com>
2025-09-08 14:34:53 +0800

    实现了sort function

--- test.cpp
+++ test.cpp
@@ -1,15 +1,27 @@
 #include<iostream>
 
 using namespace std;
-
-int sort(int arr[], int size)
+void sort(int arr[], int size)
 {
-
+	int temp = 0;
+	for (int i = 0; i < size - 1; i++)
+	{
+		for (int j = 0; j < size - 1; j++)
+		{
+			if (arr[j] > arr[j + 1])
+			{
+				temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+	}
 }
 
 int main()
 {
 	int arr[] = { 21,41,53,65,77,33,52, };
 	int size = sizeof(arr) / sizeof(arr[0]);
+	sort(arr, size);
 	return 0;
 }
