8995eb5bd53aad027179d4c8ee24dcd50db147ee
unknown <3345920771@qq.com>
2025-09-08 14:03:53 +0800

    创建了test.cpp

---
+++ test.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int sort(int arr[], int size)
+{
+
+}
+
+int main()
+{
+	int arr[] = { 21,41,53,65,77,33,52, };
+	int size = sizeof(arr) / sizeof(arr[0]);
+	return 0;
+}
